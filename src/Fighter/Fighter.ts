98e6ee05f3282c3ae408807f9612ae19f17071c5
WeydsonCristiano <weydsoncristiano@hotmail.com>
2023-02-16 20:14:39 -0300

    fix correcao detalhe Interface Figth

--- src/Fighter/Fighter.ts
+++ src/Fighter/Fighter.ts
@@ -1,7 +1,10 @@
+import Energy from '../Energy';
+
 interface Fighter {
   lifePoints: number;
   strength: number;
   defense: number;
+  energy?: Energy;
   attack(enemy: Fighter): void;
   special(enemy: Fighter): void;
   levelUp(): void;
