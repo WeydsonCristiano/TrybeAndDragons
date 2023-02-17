fa892d7d305e07783c2799aca87deebd401b78d6
WeydsonCristiano <weydsoncristiano@hotmail.com>
2023-02-17 11:38:57 -0300

    feat: criacao class Monster

---
+++ src/Monster.ts
@@ -0,0 +1,32 @@
+import { SimpleFighter } from './Fighter';
+
+class Monster implements SimpleFighter {
+  private _lifePoints: number;
+  private _strength: number;
+
+  constructor() {
+    this._lifePoints = 85;
+    this._strength = 63;
+  }
+
+  get lifePoints(): number {
+    return this._lifePoints;
+  }
+
+  get strength(): number {
+    return this._strength;
+  }
+
+  attack(enemy: SimpleFighter): void {
+    enemy.receiveDamage(this._strength);
+  }
+
+  receiveDamage(attackPoints: number): number {
+    const damage = attackPoints - this._lifePoints;
+    if (damage <= 0) this._lifePoints = -1;
+    if (damage > 0) this._lifePoints = damage;
+    return this._lifePoints;
+  }
+}
+
+export default Monster;
