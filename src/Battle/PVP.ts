dedd63339f197a3f8b31cf6292d9fbb17f7a89c4
WeydsonCristiano <weydsoncristiano@hotmail.com>
2023-02-17 14:06:07 -0300

    feat criacao da battle pvp

---
+++ src/Battle/PVP.ts
@@ -0,0 +1,22 @@
+import Battle from './Battle';
+import Fighter from '../Fighter';
+
+class PVP extends Battle {
+  private _player2: Fighter;
+  constructor(player1: Fighter, player2: Fighter) {
+    super(player1);
+    this._player2 = player2;
+  }
+
+  fight(): number {
+    let battleOn = true;
+    while (battleOn) {
+      if (this.player.lifePoints === -1) battleOn = false;
+      this.player.attack(this._player2);
+      if (this._player2.lifePoints === -1) battleOn = false;
+      this._player2.attack(this.player);
+    }
+    return super.fight();
+  }
+}
+export default PVP;
